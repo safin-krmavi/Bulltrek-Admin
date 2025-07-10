5ed30d8fe11024b279669581ade251e497bb913e
safin-krmavi <safin@krmavi.com>
2025-07-10 23:35:07 +0530

    fix(Login):logo fix

--- src/layout/auth-layout.tsx
+++ src/layout/auth-layout.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Logo } from "@/pages/login/logo";
 
 const AuthLayout = ({children}:{children:React.ReactNode}) => {
   return (
     <div className='flex flex-row h-screen w-screen'>
         <div className='w-2/6 h-full bg-secondary-50 dark:bg-[#18181b] p-20 transition-colors duration-300'>
-            <img src="/logo.svg" alt="logo" className='aspect-square w-[180px]'/>
+            <Logo width={200} height={100} className='w-[200px] drop-shadow-lg' />
             <img src="/login.svg" alt="login logo" />
         </div>
         <div className='w-4/6 h-full bg-background text-foreground transition-colors duration-300'>
